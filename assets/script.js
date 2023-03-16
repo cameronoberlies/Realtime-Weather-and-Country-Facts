2de1a65ce0a01d6158626ee2b4b2370d29e0eb15
Cameron Oberlies <cameron.oberlies@outlook.com>
2023-03-15 19:27:45 -0500

    tried to fix population and language bug, no success

--- assets/script.js
+++ assets/script.js
@@ -9,28 +9,31 @@ submitBtn.addEventListener("click", function () {
     })
     .then(function (data) {
       console.log(data);
-      var currenciesDiv = document.createElement('div');//Creates a <div> for the given parameter
-        currenciesDiv.setAttribute('id', 'currency');//Once the div is created it gives it a specific ID
+      var currenciesDiv = document.createElement('div');
+        currenciesDiv.setAttribute('id', 'currency');
         currenciesDiv.textContent = data[0].currencies[Object.keys(data[0].currencies)[0]].name;
-      var capitalDiv = document.createElement('div');//Creates a <div> for the given parameter
-        capitalDiv.setAttribute('id', 'capital')//Once the div is created it gives it a specific ID
+      var capitalDiv = document.createElement('div');
+        capitalDiv.setAttribute('id', 'capital')
         capitalDiv.textContent = data[0].capital[0]
-      var languageDiv = document.createElement('div');//Creates a <div> for the given parameter
-        languageDiv.setAttribute('id', 'language')//Once the div is created it gives it a specific ID
-        //languageDiv.textContent = data[0].languages[Object.keys(data[0].languages)][0].name
-        languageDiv.textContent= data[0].languages[Object.keys(data[0].languages)].name;
-      var mapsDiv = document.createElement('div');//Creates a <div> for the given parameter
-        mapsDiv.setAttribute('id', 'maps');//Once the div is created it gives it a specific ID
+      var languageDiv = document.createElement('div');
+        languageDiv.setAttribute('id', 'language')
+        languageDiv.textContent= data[0].languages;//Languages not displaying on page
+      var mapsDiv = document.createElement('div');
+        mapsDiv.setAttribute('id', 'maps');
         mapsDiv.textContent = data[0].maps.googleMaps;
-      var regionDiv = document.createElement('div');//Creates a <div> for the given parameter
-        regionDiv.setAttribute('id', 'region');//Once the div is created it gives it a specific ID
+      var regionDiv = document.createElement('div');
+        regionDiv.setAttribute('id', 'region');
         regionDiv.textContent = data[0].region;
+      var populationDiv = document.createElement('div');
+        populationDiv.setAttribute('id', 'population');
+        populationDiv.textContent = data[0].population;//Population not displaying on page
       var container1 = document.querySelector('.container');
         container1.appendChild(currenciesDiv);
         container1.appendChild(capitalDiv);
         container1.appendChild(languageDiv);
         container1.appendChild(mapsDiv);
         container1.appendChild(regionDiv);
+        container1.appendChild(populationDiv);
     
     });
 });
